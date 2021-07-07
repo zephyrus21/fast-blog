b5479dbb97a77cc50f7d45d7e8fea9f0c254ee43
zephyrus <pandey.piyus@gmail.com>
2021-07-07 19:43:04 +0530

    adding firebase auth github 🧀

---
+++ pages/index.jsx
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+import { useAuth } from '../lib/auth';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  const auth = useAuth();
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <button onClick={(e) => auth.signinWithGitHub()}>Sign In</button>
+      <div>{auth?.user?.email}</div>
+    </div>
+  );
+}
