221a9ff64a3cd7393b82153c8f288358782075e6
zephyrus <pandey.piyus@gmail.com>
2021-07-08 18:55:30 +0530

    adding dashboard

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,28 @@
-import { Button, Heading, Text } from '@chakra-ui/core';
+import Head from 'next/head';
+import { Box, Button, Flex, Icon, Text } from '@chakra-ui/core';
 import { useAuth } from '../lib/auth';
 
 export default function Home() {
   const auth = useAuth();
 
   return (
-    <div>
-      <Text>{auth.user?.email}</Text>
-      {auth.user ? (
-        <Button onClick={(e) => auth.signout()}>Sign Out</Button>
-      ) : (
-        <Button onClick={(e) => auth.signinWithGitHub()}>Sign In</Button>
-      )}
-    </div>
+    <>
+      <Head>
+        <title>Fast Blog</title>
+      </Head>
+      <Flex direction='column' align='center' justify='center' h='100vh'>
+        <Icon name='logo' size='32px' />
+        <Text>{auth.user?.email}</Text>
+        {auth.user ? (
+          <Button size='sm' onClick={(e) => auth.signout()}>
+            Sign Out
+          </Button>
+        ) : (
+          <Button size='sm' onClick={(e) => auth.signinWithGitHub()}>
+            Sign In
+          </Button>
+        )}
+      </Flex>
+    </>
   );
 }
